8bfcc413ee8df101ac60d03a9982bf3a4baca7bb
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-02 13:37:01 -0700

    RUN

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("Size of a char: ");
+	printf("Size of an int: ");
+	printf("Size of a long int: ");
+	printf("Size of a long long int: ");
+	printf("Size of a float: ");
+
+	return 0;
+}

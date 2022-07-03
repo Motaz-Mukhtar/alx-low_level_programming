60e4ae99616c81de79b22caa109b122c4801d792
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-03 14:37:51 -0700

    RUN

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main(void) 
+/**
+ * main - This this is the strting pointer of the program
+ * Return: main function
+ */
+int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+/**
+ * main - This is the starting point of the program
+ * Return: main function
+ */
 
-int main(void) 
+int main(void)
+
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
 }

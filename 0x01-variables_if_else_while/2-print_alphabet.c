e8253a8e5789f4662a347d54d409c322e8a5ecd9
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-03 03:00:49 -0700

    RUN

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	putchar("abdcefghijklmnopqrstuvwxyz");
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int n = 0;
+	while(n < 10)
+	{
+		printf("%d",n);
+		n++;
+	}
+	return (0);
+}

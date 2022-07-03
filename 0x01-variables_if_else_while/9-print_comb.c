67bc848db9e4a2b5693edfb987d920939d906c69
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-03 14:16:36 -0700

    RUN

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
-
-int main(void) 
+/**
+ * main - This is the start point of the program
+ * Return: main function
+ */
+int main(void)
 {
 	int n = '0';
-	while (n <= 9) 
+
+	while (n <= '9')
 	{
 		putchar(n);
+		if (n < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 		n++;
 	}
+	putchar('\n');
 	return (0);
+
 }

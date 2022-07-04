f7a1639b5f5dae4b9368bc507378224591884f84
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-04 15:49:15 -0700

    RU

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ * Return: main fucntion
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19.\n");
+	return (1);
+}

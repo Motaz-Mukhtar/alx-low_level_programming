7960030cce9a5ec3bc4a7272360a338ff303e068
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-04 15:50:19 -0700

    RUN

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19.\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19.");
 	return (1);
 }

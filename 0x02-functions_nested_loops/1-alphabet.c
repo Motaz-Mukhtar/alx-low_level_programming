bd950ae455c62498c1a3206ff2799b071ab351d4
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 09:20:49 -0700

    RUN

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - print alphabet, in lowercase
+ * Return: Always 0..
+ */
+void print_alphabet(void)
+{
+	int n = 'a';
+
+	while (n <= 'z')
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+}
 /**
  * main - Entry point of the program
  * Return: main function

440c42181fa1f511b398d57620fc01582601534f
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 14:31:01 -0700

    run

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,18 @@
-#include <stdio.h>
-/**
- * sayHello - print _putchar followed by new line
- * Return: Always 0.
- */
-int sayHello(void)
-{
-	printf("_putchar\n");
-	return (0);
-}
-
+#include "main.h"
 /**
  * main - Entry point of the program
  * Return: main function
  */
 int main(void)
 {
-	sayHello();
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }

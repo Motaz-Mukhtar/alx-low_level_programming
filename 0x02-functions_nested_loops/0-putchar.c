62520a2cb5f81579820754638d0f4c12aa84e550
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 07:28:17 -0700

    RUN

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entery Point
+ * syaHello - print _putchar
+ * Return: main function
+*/
+int sayHello(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
+
+int main(void)
+{
+	sayHello();
+	return (0);
+}

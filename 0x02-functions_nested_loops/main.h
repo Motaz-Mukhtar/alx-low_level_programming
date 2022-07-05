54069b52b274150e7b208327f9b0294b33e7abda
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 14:32:08 -0700

    run

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,9 +7,6 @@
  * on error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
 /**
  * print_alpahbet_x10 - print the alphabet, in lowercase
  * Return - Always 0.

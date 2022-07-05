65aba19c75944bd25c4e323d1fbf300c80877498
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 15:31:02 -0700

    run

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: integer type.
+ * Return: Always 0.
+ */
+int print_last_digit(int n)
+{
+	n = n % 10;
+	return (n);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -102,5 +102,5 @@ int print_sign(int n);
 	}
 }*/
 int _abs(int);
-
+int print_last_digit(int);
 

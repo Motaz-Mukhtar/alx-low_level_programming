f8a023320c6ef45b186112268f83690da951e293
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 08:02:31 -0700

    run

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,5 @@
 #include <stdio.h>
 /**
- * sayHello - Print _putchar with a new line
- * Return: Always 0.
- */
-int sayHello(void)
-{
-	printf("_putchar");
-	return (0);
-}
-
-/**
  * print_alpahbet - print the alphabet, in lowercase
  * Return - Always 0.
  */

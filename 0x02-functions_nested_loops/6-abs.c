3f59633bb80f7ba8d29b4ab200320e2e554abcb8
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-05 15:27:42 -0700

    run

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * _abs - absolute value of an integer.
- * @c - integer type
+ * @c: integer type
  * Return: Always 0.
  */
 int _abs(int c)
 {
 	if (c < 0)
 	{
-		c += c + c;
+		c -= c + c;
 	}
 	return (c);
 }

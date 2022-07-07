7356f09af969c95c5836f2e55bb340b7550f49ed
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 05:00:04 -0700

    run

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program 
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 10;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}

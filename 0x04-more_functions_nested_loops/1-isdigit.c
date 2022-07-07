51ecdddb3ad0f8ca398c2976144afaded170c1d5
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 05:21:24 -0700

    run

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - Checks for a digit
+ * @c: int type
+ * Return Always 0.
+ */
+int _isdigit(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

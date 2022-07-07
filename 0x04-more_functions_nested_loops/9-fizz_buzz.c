83700f1431e3872d5c36bd9fdb54e381b3bc9ef4
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 16:22:33 -0700

    run

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point of the program
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 1;
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}

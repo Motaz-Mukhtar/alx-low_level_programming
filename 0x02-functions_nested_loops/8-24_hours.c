36ae77afa01fc302ef660855e5965c0d2e69c786
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-06 17:42:08 -0700

    run

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,40 +1,44 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * jack_bauer - prints every minute of the day
  * Return: Always 0.
  */
 void jack_bauer(void)
 {
-	int a = '0', b = '0';
+	int a = 0, b = 0;
 
 	while (a <= 23)
 	{
-		if (b <= 9 && a <= 0)
+		if (b <= 9 && a <= 9)
 		{
-			_putchar('0');
-			_putchar(a);
-			_putchar(':');
-			_putchar('0');
-			_putchar(b);
-			_putchar('\n');
+			printf("0%d:0%d\n", a, b);
+		//	_putchar('0');
+		//	_putchar(a);
+		//	_putchar(':');
+		//	_putchar('0');
+		//	_putchar(b);
+		//	_putchar('\n');
 		}
 		else
 		{
 			if (b > 9 && a <= 9)
 			{
-				_putchar('0');
-				_putchar(a);
-				_putchar(':');
-				_putchar(b);
-				_putchar('\n');
+				printf("0%d:%d\n", a, b);
+			//	_putchar('0');
+			//	_putchar(a);
+			//	_putchar(':');
+			//	_putchar(b);
+			//	_putchar('\n');
 			} else
 			{
 				if (b > 9 && a > 9)
 				{
-					_putchar(a);
-					_putchar(':');
-					_putchar(b);
-					_putchar('\n');
+					printf("%d:%d", a, b);
+			//		_putchar(a);
+			//		_putchar(':');
+			//		_putchar(b);
+			//		_putchar('\n');
 				}
 			}
 		}

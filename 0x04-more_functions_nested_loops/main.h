e3ee97ef8053c3ac566c322173144538b080c7b8
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 06:18:02 -0700

    run

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,17 +7,12 @@
  * On error, -1 is returned, and errno is set approperiately.
  */
 int _putchar(char c);
-//{
+/*{
 //	return (write(1, &c, 1));
 //}
-
-
+*/
 int _isupper(int c);
-
 int _isdigit(int c);
-
 int mul(int a, int b);
-
 void print_numbers(void);
-
 void print_most_numbers(void);

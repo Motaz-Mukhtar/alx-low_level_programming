b3cabc1860cdf9a3d057bb0c6cd0503d1c68589a
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 04:42:47 -0700

    run

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,37 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * positive_or_negative - is the number @n pos or neg
+ * Return: Always 0.
+ */
+void positive_or_negative(int n)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	}
+	if (n < 0)
+	{
+		printf("%d is negative", n);
+	}
+	if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+}
 /**
  * main - Entry point of the program
  * Return: Always 0.
  */
 int main(void)
 {
-	if (positive_or_negative(0))
-	{
-		positive_or_negative();
-	}
-	else
-	{
-		printf("Must be 0\n");
-	}
+	positive_or_negative(0);
 	return (0);
 }

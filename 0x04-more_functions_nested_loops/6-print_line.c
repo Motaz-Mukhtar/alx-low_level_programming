cb8631c75a337f996bc0989834fbe6932fbde768
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 07:26:40 -0700

    run

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_line - draws a straight line
+ * @n: int type
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int j = 0;
+
+	while (j < n)
+	{
+		if (n > 0)
+		{
+			putchar('_');
+		}
+		j++;
+	}
+	putchar('$');
+	putchar('\n');
+}

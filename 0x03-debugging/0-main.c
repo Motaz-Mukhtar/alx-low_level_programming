ba5609a3f7ae511ac462fe932deea949c297744f
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-08 05:05:48 -0700

    run

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,35 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * positive_or_negative - is the number @n pos or neg
- * @i: integar type.
+ * main - Entry point of the program
  * Return: Always 0.
  */
-void positive_or_negative(int i)
-{
-	int n;
-
-	srand(time(0));
-	n = i;
-
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
-	if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-}
-
 int main(void)
 {
-	positive_or_negative(3);
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
 	return (0);
 }

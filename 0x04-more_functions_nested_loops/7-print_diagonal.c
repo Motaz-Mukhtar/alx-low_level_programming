069b141368198bf02a12971af278ec1ba5bfa1fa
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 17:55:14 -0700

    hot fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,26 +7,18 @@
  */
 void print_diagonal(int n)
 {
-	int j, i;
+	int x, i;
 
 	if (n <= 0)
-	{
 		_putchar('\n');
-	}
-	else if (n > 0)
+	else
 	{
-		for (j = 0; j < n; j++)
+		for (x = 0; x < n; x++)
 		{
-			for (i = 0; i < j; i++)
-			{
+			for (i = 0; i < x; i++)
 				_putchar(' ');
 				_putchar(92);
 				_putchar('\n');
-			}
 		}
-	}
-	else
-	{
-		_putchar('\n');
 	}
 }

0acb4c9b81fe9cda4ff1add8d18a47c82a9290b5
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-07 17:43:03 -0700

    run

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,22 +13,16 @@ void print_diagonal(int n)
 	{
 		_putchar('\n');
 	}
-	else	
+	else
 	{
 		for (j = 0; j < n; j++)
 		{
 			for (i = 0; i <= j; i++)
 			{
-				if (n > 0)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
+				_putchar(92);
+				_putchar('\n');
 			}
-			if (n > 0)
-			{
-				_putchar('\\');
-			}
-			_putchar('\n');
 		}
 	}
 }

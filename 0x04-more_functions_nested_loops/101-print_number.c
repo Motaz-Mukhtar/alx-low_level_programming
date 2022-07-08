e229421581ff842f60446aefb093c5eea4865782
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-08 04:57:12 -0700

    run

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_number - function that prints an integer.
+ * @n: int type
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		putchar('-');
+		n = -n;
+	}
+	if (n == 0)
+	{
+		putchar('0');
+	}
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+	putchar(n % 10 + '0');
+}
--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,6 +1,25 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * printnumber - print int
+ * @n: int type
+ * Return: Always 0.
+ */
+void printnumber(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10)
+	{
+		printnumber(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
+
+/**
  * more_numbers - prints 10 times the numbers
  * Return: Alwyas 0.
  */
@@ -12,7 +31,7 @@ void more_numbers(void)
 	{
 		for (n = 0; n < 15; n++)
 		{
-			_putchar(n + '0');
+			printnumber(n);
 		}
 		_putchar('\n');
 	}

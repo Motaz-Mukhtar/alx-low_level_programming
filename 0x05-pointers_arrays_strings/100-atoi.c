6dec48ec929eadc16a2daa148484c0dc298fcac2
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-12 16:48:40 -0700

    hotFix

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _atoi - conver a string to int
+ * @s: char type
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	int val;
+
+	val = atoi(s);
+	if (val == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (val);
+	}
+}

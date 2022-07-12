5cd62a42ce1579896487d0463cbab18bfa43ed14
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-12 08:38:55 -0700

    hotFix

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - copy the string pointed
+ * @dest: char type
+ * @src: char type
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}

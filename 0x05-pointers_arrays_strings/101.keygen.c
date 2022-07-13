866789cf4cef2068d1cd4e1e26d8769fc4926e4b
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-12 17:19:01 -0700

    run

---
+++ 0x05-pointers_arrays_strings/101.keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+/**
+ * main - This is the starting point of the program
+ * Return: main function
+ */
+int main(void)
+{
+	char chars[]
+	=
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*";
+	char n;
+
+	srand(time(0));
+
+	for (int i = 0; i <= 10; i++)
+	{
+		n = 0 + (rand() % (strlen(chars) - 1));
+		printf("%c", chars[n]);
+	}
+	printf("\n");
+	return (0);
+}

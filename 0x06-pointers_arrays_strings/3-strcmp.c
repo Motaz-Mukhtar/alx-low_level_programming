5aefa5d5e57a749e1385dad97c98ee20b14839e9
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-13 12:39:04 -0700

    run

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - compares two strings.
+ * @s1: char type
+ * @s2: char type
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}

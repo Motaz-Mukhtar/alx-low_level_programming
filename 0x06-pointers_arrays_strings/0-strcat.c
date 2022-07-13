dc3f518b4bbf9432dc95607e774865ed9520aae6
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-13 07:34:05 -0700

    run

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: char type
+ * @src: char type
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+}

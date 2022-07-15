e239377b108364e44dad1893be163962a217cb7c
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-14 19:18:45 -0700

    hotfix

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * cap_string - capitalizes all words of a string
+ * @c: char type
+ * Return: Always 0.
+ */
+char *cap_string(char *c)
+{
+	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(',
+	')', '{', '}'};
+	int len = 13;
+	int a = 0, i;
+
+	while (c[a])
+	{
+		i = 0;
+		while (i < len)
+		{
+			if ((a == 0 || c[a - 1] == spc[i]) && (c[a] >= 97
+			&& spc[a] <= 122))
+			{
+				c[a] = c[a] - 32;
+			}
+			i++;
+		}
+		a++;
+	}
+	return (c);
+}

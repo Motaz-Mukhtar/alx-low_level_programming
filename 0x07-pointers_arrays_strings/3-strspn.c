bcc2b01ef03d5843124bcffef76205d1793c7c1e
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-18 14:55:15 -0700

    htFix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strspn - gts the lngth of perfix substring
+ * @s: char type
+ * @accept: char type
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accpet));	
+}

f1341e5bf40b1650511626f806b1e55f77e9fe69
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-18 09:30:06 -0700

    run

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _strchr - locates a char in a string
+ * @s: char type
+ * @c: char ttype
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}

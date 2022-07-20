5305e456967ed5ee9d9e2e9215fcd53780df033e
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-20 08:36:39 -0700

    hotFix

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: char type
+ * Return: Always 0.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	s++;
+	_puts_recursion(s);
+	s--;
+	_putchar(*s);
+}

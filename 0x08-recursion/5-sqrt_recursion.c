69a2095efaf4ebf1626f668c7898eb650317292b
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-21 04:27:55 -0700

    hotFix

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,21 @@
  * @n: int type
  * Return: Always 0.
  */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+	{
+		return (-1);
+	}
+	if (square == n)
+	{
+		return (i);
+	}
+	return (_sqrt(n, i + 1));
+}
 int _sqrt_recursion(int n)
 {
-
+	return (_sqrt(n, 1));
 }

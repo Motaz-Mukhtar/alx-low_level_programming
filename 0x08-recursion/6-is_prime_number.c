3f2dfa4bff83a0041269ace75752815844a95503
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-21 05:01:49 -0700

    hotfix

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,15 +9,11 @@
 int prime(int n, int i)
 {
 	if (n <= 1)
-	{
 		return (0);
-	}
 	if (n % i == 0 && i > 1)
-	{
 		return (0);
 	if ((n / i) < i)
 		return (1);
-	}
 	return (prime(n, i + 1));
 }
 /**

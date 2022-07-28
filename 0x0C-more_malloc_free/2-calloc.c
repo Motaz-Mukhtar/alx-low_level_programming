b24ac31a8cb8618db6cb58f493914d9d5a75d5a9
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-28 15:35:04 -0700

    run

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,6 +10,13 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *p;
+	
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
 	return (calloc(nmemb, size));
 }
 

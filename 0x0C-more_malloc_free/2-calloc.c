cd9b1ba0d1041c186e8a04165767ea31fbeae416
Motaz-Mukhtar <motazksa2@gmail.com>
2022-07-28 15:45:01 -0700

    hotFix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,7 +12,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
 
-	p = malloc(nmemb * size);
+	p = (int *)malloc(nmemb);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (p == NULL)

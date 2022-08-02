7000766402d9149bfa819aaa4843070adc9e64da
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-02 16:12:45 -0700

    hotFxig

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,10 +1,9 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - frees dogs
  * @d: dog_t type
- * Return: ALways 0.
+ * Retrun: Always 0.
  */
 void free_dog(dog_t *d)
 {

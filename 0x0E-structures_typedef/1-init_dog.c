d534aad7ec0c87f45459b1f5c69560c00589aa53
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-02 05:39:38 -0700

    run

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,8 +10,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d->name == NULL)
-		d->nmae = " ";
+	if (d.name == NULL)
+		d->name = " ";
 	d->name = name;
 	d->age = age;
 	d->owner = owner;

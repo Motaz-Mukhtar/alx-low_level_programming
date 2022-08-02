7bda888ba3935b40625a4b7a16550e4da1fcd379
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-02 16:09:07 -0700

    hotFix

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * new_dog - creates a new dog.
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if ()
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * free_dog - frees dogs
+ * @d: dog_t type
+ * Return: ALways 0.
+ */
+void free_dog(dog_t *d)
+{
+	free(d);
+}

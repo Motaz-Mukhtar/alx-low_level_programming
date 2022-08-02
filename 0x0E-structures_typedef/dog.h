87a0e1b3715634af0ade2e6a47fd97427efbb9ff
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-02 05:36:02 -0700

    rn

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-#endif
-struct dog {
+/**
+ * struct dog - first struct
+ * @name: char type
+ * @age: float type
+ * @owner: char type
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+#endif

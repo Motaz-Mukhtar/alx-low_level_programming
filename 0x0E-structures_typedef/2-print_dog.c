f5815abae20c08febb9a346eeea2a7290f316ccd
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-02 15:47:24 -0700

    run

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -17,14 +17,7 @@ void print_dog(struct dog *d)
 		{
 			printf("Name: %s\n", d->name);
 		}
-//		if (d->age == NULL)
-//		{
-//			printf("Age: (nil)\n");
-//		}
-//		else
-//		{
 		printf("Age: %f\n", d->age);
-//		}
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)\n");

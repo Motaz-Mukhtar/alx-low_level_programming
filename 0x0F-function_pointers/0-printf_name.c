a71ba1e2abc1242e3287e9098bc40185471f99a3
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-03 04:08:31 -0700

    run

---
+++ 0x0F-function_pointers/0-printf_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+/**
+ * print_name - fucntion that prints a name;
+ * @name: char type
+ * @f: void fucntion to pointer type
+ * Return: Always 0.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	void (*func)(char *);
+
+	func = f;
+	func(name);
+}

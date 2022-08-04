36a6cd8497810748c40e6d5cd951d9ce17b9ff2d
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-04 16:01:32 -0700

    run

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,6 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		char *str = va_arg(ap, char *);
+
 		if (str == NULL)
 			str = "(nil)";
 		if (i == n - 1 || separator == NULL)

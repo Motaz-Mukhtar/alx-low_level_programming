b0427e8cc71171afd864bf327cb270a66dad58aa
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-04 06:06:48 -0700

    run

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -13,6 +13,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start (ap, coutn);
 
 	sum = 0;
+	if (n == 0)
+		return (0);
 	for (i = 0; i < coutn; i++)
 		sum += var_arg (ap, int);
 	va_end(ap);

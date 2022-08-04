2cbf7c535c3c4efee570a67072eda83f1a685a7a
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-04 15:45:30 -0700

    run

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -11,18 +11,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
+	char *str;
 
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
+		str == va_arg(ap, char *);
+		if (str == NULL)
+			str = "(nil)";
 		if (i == n - 1 || separator == NULL)
 		{
-			printf("%s", va_arg(ap, char *));
+			printf("%s", str);
 		}
 		else
 		{
 			if (i < n)
-				printf("%s%s", va_arg(ap, char *), separator);
+				printf("%s%s", str, separator);
 		}
 	}
 	printf("\n");
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_all - prints anything
+ * @format: const char const * type
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+	char *str;
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		if (i && (format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+		|| format[i] == 's'))
+			printf(", ");
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (str == NULL)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ap);
+}

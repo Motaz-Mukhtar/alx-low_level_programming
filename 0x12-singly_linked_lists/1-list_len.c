60724d6ae9b16e9e7382cf6f25567a6aebb47bf7
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-11 17:06:41 -0700

    push

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,26 +1,22 @@
 #include "lists.h"
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
 /**
- * print_list - prints all the elemnest of a list
+ * list_len - returns the number of element in list
  * @h: const list_t type
  * Return: 0.
  */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
 	int length;
 	int *p;
 
-	p = malloc(sizeof(h));
+	p = malloc(sizeof(p));
 	if (p == NULL)
 		return (0);
 	while (h)
 	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		length++;
 	}

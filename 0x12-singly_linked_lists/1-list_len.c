804e1e556bb70923175ecdf9c04cc6e5073ac5ac
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-11 17:12:13 -0700

    run

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -12,11 +12,12 @@ size_t list_len(const list_t *h)
 	int length;
 	int *p;
 
-	p = malloc(sizeof(p));
-	if (p == NULL)
-		return (0);
 	while (h)
 	{
+		p = malloc(sizeof(h));
+		if (p == NULL)
+			return (0);
+
 		h = h->next;
 		length++;
 	}

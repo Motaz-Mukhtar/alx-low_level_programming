eb4b547dab0be17ac8aaa3fb421ba19bfbbf521c
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-11 17:18:14 -0700

    hotfix

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,11 +10,16 @@
 size_t list_len(const list_t *h)
 {
 	int length;
+	int *p;
 
+	p = malloc(sizeof(h));
+	if (p == NULL)
+		return (0);
 	while (h)
 	{
 		h = h->next;
-		length++;
+		if (h->next != NULL)
+			length++;
 	}
 	return (length);
 }

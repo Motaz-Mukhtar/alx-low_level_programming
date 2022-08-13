31e45e139adae0750ac5ce0e2fb8f7e1e3d498e8
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-12 17:15:27 -0700

    run

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * free_list - frees a list_t list
+ * Return: 0.
+ */
+void free_list(list_t *head)
+{
+	free(head);
+}

64ee8ba2a76cdbe85eaa1de5f4563ed7bedba77f
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-12 19:26:56 -0700

    hotfix

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 /**
  * free_list - frees a list_t list
  * @head: list_t type
@@ -9,10 +8,12 @@
  */
 void free_list(list_t *head)
 {
-	list_t *tmp;
+	list_t *tmp = head;
+
 	while (tmp != NULL)
 	{
 		list_t *n1 = tmp;
+	
 		tmp = tmp->next;
 		free(n1);
 	}

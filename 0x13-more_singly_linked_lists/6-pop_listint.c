582aa0bf95813431f1a469d4c3c23750b900a462
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-15 10:47:26 -0700

    hotfix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
 /**
  * pop_listint - deletes the head node of list
@@ -9,9 +10,10 @@
 int pop_listint(listint_t **head)
 {
 	listint_t *tmp = malloc(sizeof(listint_t));
-	tmp = *head;
-	int num = tmp->n;
+	int num;
 
+	tmp = *head;
+	num = tmp->n;
 	if (*head == NULL)
 		return (0);
 	*head = tmp->next;

a382ab95a76a0ab133bd21b2487cce13c3ed5801
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-16 01:42:53 -0700

    hotfx

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,10 +9,8 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
-	int *p;
 
-	p = malloc(sizeof(head));
-	if (p == NULL)
+	if (*head == NULL)
 		head = NULL;
 	tmp = *head;
 	while (*head != NULL)

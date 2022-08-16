5510873fdc43e6bf23c44fc8d0ab214511ad3d29
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-16 14:46:09 -0700

    hotfix

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,8 +10,6 @@ int sum_listint(listint_t *head)
 {
 	int sum;
 
-	if (head == NULL)
-		return (0);
 	while (head != NULL)
 	{
 		sum += head->n;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,31 +9,30 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new = malloc(sizeof(listint_t));
-	unsigned int i = 0;
+	listint_t *new = malloc(sizeof(listint_t)), *temp = *head;
+	unsigned int i;
 
-	if (head == NULL)
-		return (NULL);
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	if (*head == NULL)
 		*head = new;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+	}
 	else
 	{
-		listint_t *tmp = *head;
-
-		while (tmp->next != NULL)
+		for (i = 0; i < (idx - 1); i++)
 		{
-			if (i == idx)
-			{
-				new->next = tmp->next;
-				tmp->next = new;
-			}
-			tmp = tmp->next;
-			i++;
+			if (temp == NULL || temp->next == NULL)
+				return (NULL);
+			temp = temp->next;
 		}
+		new->next = temp->next;
+		temp->next = new;
 	}
 	return (*head);
 }

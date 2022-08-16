cfa3bed498e70c85261ea719f7d3e65e5e410e7a
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-16 01:46:11 -0700

    hotfix

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,8 +10,6 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
-		head = NULL;
 	tmp = *head;
 	while (*head != NULL)
 	{
--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,10 +12,10 @@ int pop_listint(listint_t **head)
 	listint_t *tmp = malloc(sizeof(listint_t));
 	int num;
 
+	if (head == NULL)
+		return (0);
 	tmp = *head;
 	num = tmp->n;
-	if (*head == NULL)
-		return (0);
 	*head = tmp->next;
 	return (num);
 }

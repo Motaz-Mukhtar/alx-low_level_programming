1cf0fc67c250273f728ec6b843420c7969b00adc
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-17 14:41:19 -0700

    hotfix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -15,7 +15,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index == 0)
 	{
-		*head = temp->next;
+		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}

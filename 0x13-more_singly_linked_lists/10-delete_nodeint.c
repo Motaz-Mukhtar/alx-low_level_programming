8099c49ee356a2d2bebd29329a76678dc08cf8e6
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-17 15:05:06 -0700

    hotfi

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,6 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	if (temp == NULL)
 		return (-1);
+
 	if (index == 0)
 	{
 		*head = (*head)->next;
@@ -23,10 +24,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	{
 		if (temp->next == NULL)
 			return (-1);
+
 		temp = temp->next;
 	}
 	temp2 = temp->next;
 	temp->next = temp2->next;
-	free(temp);
+	free(temp2);
 	return (1);
 }

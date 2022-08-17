1957d97fa4e2599f053da1a4ee5febff39123f65
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-17 14:37:45 -0700

    hotif

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -8,7 +8,7 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *temp = *head;
+	listint_t *temp = *head, *temp2;
 	unsigned int i;
 
 	if (temp == NULL)
@@ -25,7 +25,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			return (-1);
 		temp = temp->next;
 	}
-	temp->next = temp->next->next;
+	temp2 = temp->next;
+	temp->next = temp2->next;
 	free(temp);
 	return (1);
 }

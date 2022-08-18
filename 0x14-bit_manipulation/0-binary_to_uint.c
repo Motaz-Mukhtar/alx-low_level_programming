ea6c362543cdfd3f68b7f1f5742e5ccc9d012370
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-18 06:11:07 -0700

    hotfix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,8 +12,9 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = len; i >= 0; i--)
 	{
+		if (b[i] == '\0')
+			return (0);
 		if (b[i] == '1' || b[i] == '0')
-		
 			number = number + (num * (b[i] - '0'));
 		else
 			return (0);

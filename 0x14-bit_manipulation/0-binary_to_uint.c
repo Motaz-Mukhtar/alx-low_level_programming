38369f2abe062ec8f9f8d28614b08719f42ef446
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-18 08:21:12 -0700

    hotifx

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,10 +10,10 @@ unsigned int binary_to_uint(const char *b)
 	int i, num = 1, len = strlen(b) - 1;
 	unsigned int number = 0;
 
+	if (b[0] == '\0')
+		return (0);
 	for (i = len; i >= 0; i--)
 	{
-		if (b[i] == '\0')
-			return (0);
 		if (b[i] == '1' || b[i] == '0')
 			number = number + (num * (b[i] - '0'));
 		else
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <unistd.h>
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif

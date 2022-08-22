ceb203547aabf5787b6cea6c08b85dac25d62f17
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-22 04:45:48 -0700

    hotfix

---
+++ 0x15-file_io/0-reak_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * read_textfile - reads a text file and prints it
+ * @filename: const char type
+ * @letters: size_t type
+ * Return: the actual number of letters itcould read
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t fd, wr, rd;
+	char *buff = (char *)malloc(sizeof(char *) * letters);
+
+	if (filename == NULL)
+		return (0);
+	if (buff == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
+		return (0);
+	rd = read(fd, buff, letters);
+	if (rd < -1)
+		return (0);
+
+	wr = write (STDOUT_FILENO, buff, rd);
+	if (wr < 0 || wr != rd)
+		return (0);
+
+	free(buff);
+	close(fd);
+	return (wr);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

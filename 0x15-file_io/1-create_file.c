e846faa22c167985a40a2b8b346a6f3b3f5f792b
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-23 04:50:40 -0700

    htofix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len]; )
 			len++;
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(fd, text_content, len);
-	if (fd < 0 || wr < 0)
+	if (fd == -1 || wr == -1)
 		return (-1);
 
 	close(fd);

dceea8479a6e045bc9f573675b05148c88b79fab
Motaz-Mukhtar <motazksa2@gmail.com>
2022-08-24 13:45:59 -0700

    hotfix

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * close_file - closeing the giving file
+ * @file: the giving file.
+ * Return: 1 on Success -1 on failure
+ */
+void close_file(int file)
+{
+		int num;
+
+		num = close(file);
+		if (num == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
+		}
+}
+/**
+ * get_buffer - get the buffer of the file
+ * file: the file
+ * Return: the buffer
+ */
+
+char *get_buffer(char *file)
+{
+	char *p;
+
+	p = malloc(sizeof(char) * 1024);
+	if (p == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+	return (p);
+}
+
+/**
+ * main - The main fucntion of the program
+ * argc: The length of the array
+ * argv: The Array
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int from, to, wr, rd;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	buffer = get_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	rd = read(from, buffer, 1024);
+	to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	while (rd > 0)
+	{
+		if (from == -1 || rd == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		wr = write(to, buffer, rd);
+		if (to == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write from to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+		rd = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+	}
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
+}

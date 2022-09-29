3864446ff2b1d820349ca19f9078e099fc5adaf2
Motaz-Mukhtar <motazksa2@gmail.com>
2022-09-29 15:45:59 -0700

    AnotherTask

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * dlistint_len - returns the number of ele in doubly linkdes list
+ * h: The doubly linked lists
+ * Return: The length of doubly linked lists
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int length = 0;
+
+	if (h == NULL)
+		return (0);
+	while (h)
+	{
+		h = h->next;
+		length++;
+	}
+	return (length);
+}
